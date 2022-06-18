d3851a3eb7d26116685b4c36c047e8be0ba32ad4
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2022-06-18 22:49:25 +0300

    game board created (#2)

---
+++ src/const/const.ts
@@ -0,0 +1,13 @@
+export let CONST = {
+    gridWidth: 9,
+    gridHeight: 11,
+    blockWidth: 45,
+    blockHeight: 50,
+    blockTypes: [
+        'blue-block',
+        'green-block',
+        'purple-block',
+        'red-block',
+        'yellow-block',
+    ]
+};
