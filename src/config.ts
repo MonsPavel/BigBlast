2a33484803c13f1d57224e48090d25785ff91872
Mons Pavel <monspavel@gmail.com>
2022-06-18 13:44:56 +0300

    add project boilerplate

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'Webpack-Boilerplate',
+  url: 'https://github.com/MonsPavel/BigBlast',
+  version: '1.0',
+  width: 800,
+  height: 600,
+  backgroundColor: 0x3a404d,
+  type: Phaser.AUTO,
+  parent: 'game',
+  scene: [],
+  render: { pixelArt: false, antialias: true }
+};
